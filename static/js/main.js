c014325b860e253a83f2ca4ad95218d22d5f2466
Ashutosh Chaudhary <ashutosh.chaudhary@appsierra.com>
2017-05-07 06:10:43 +0530

    Added search and fixed some issues

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,5 @@
 (function($){
 	$(document).ready(function(){
-		
-	
+		//Empty space. Fill this with your site specific JS code
 	});
 })(jQuery);
--- static/js/projects.js
+++ static/js/projects.js
@@ -1,22 +1,49 @@
 (function($){
 	$(document).ready(function(){
+		var selectedTags = [];
 		$(document).on('click','.tag-filter',function(){
+			console.log("Tags", selectedTags);
 			if( $(this).hasClass('all')){
+				selectedTags = [];
 				$('span.tag-filter').removeClass("label-primary");
+				$(this).addClass("label-primary");
 				$('.project-item').showAll();
 			}else{
-				$('.project-item').filterTags( $(this).data('tag') );
+				if( $(this).hasClass("label-primary") ){
+					selectedTags.removeTag( $(this).data('tag') );
+					$(this).removeClass("label-primary");
+				}
+				else{
+					selectedTags.addTag( $(this).data('tag') );
+					$(this).addClass("label-primary");
+				}
+				$('.project-item').filterTags( selectedTags );
 			}
-			$(this).addClass("label-primary");
 		});
 	});
 
+	Array.prototype.addTag = function(tag) {
+		if(this.indexOf(tag) === -1) this.push(tag);
+	}
+
+	Array.prototype.removeTag = function(tag) {
+		var tagIndex = this.indexOf(tag);
+		this.splice(tagIndex,1);
+	}
+
 	$.fn.extend({
-	  filterTags: function(tagName) {
+	  filterTags: function(tagNames) {
+	  	if(tagNames.length == 0) return this.showAll();
+
 	    return this.each(function() {
 	    	var itemTagArray = JSON.parse( $(this).attr('data-tags') );
-			if($.inArray(tagName, itemTagArray) === -1){
+	    	var unfound = $( tagNames ).not( itemTagArray ).get();
+	    	console.log("unfound", unfound, itemTagArray.length);
+	    	if( unfound.length == tagNames.length ){
+		//	if($.inArray(tagName, itemTagArray) === -1){
 				$(this).addClass('not-show');
+			}else{
+				$(this).removeClass('not-show');
 			}
 	    });
 	  },
